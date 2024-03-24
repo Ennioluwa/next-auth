f2338e28b1afb8cb89f2fe8fe4ec4afd145f5837
Enioluwa <enioluwaarowo@gmail.com>
2024-03-24 21:42:56 +0100

    add callbacks

--- auth.ts
+++ auth.ts
@@ -5,8 +5,16 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 import { getUserById } from "@/data/user";
+import { UserRole } from "@prisma/client";
 // import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
 // import { getAccountByUserId } from "./data/account";
+
+declare module "next-auth" {
+  interface User {
+    /** The user's postal address. */
+    role: UserRole;
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -27,70 +35,73 @@ export const {
       });
     },
   },
-  // callbacks: {
-  //   async signIn({ user, account }) {
-  //     // Allow OAuth without email verification
-  //     if (account?.provider !== "credentials") return true;
+  callbacks: {
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification
+      // if (account?.provider !== "credentials") return true;
+      console.log(user, "user");
 
-  //     const existingUser = await getUserById(user.id);
+      // if (!user.id) return false;
+      // const existingUser = await getUserById(user.id);
 
-  //     // Prevent sign in without email verification
-  //     if (!existingUser?.emailVerified) return false;
+      // // Prevent sign in without email verification
+      // if (!existingUser?.emailVerified) return false;
 
-  //     if (existingUser.isTwoFactorEnabled) {
-  //       const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
+      //     if (existingUser.isTwoFactorEnabled) {
+      //       const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
 
-  //       if (!twoFactorConfirmation) return false;
+      //       if (!twoFactorConfirmation) return false;
 
-  //       // Delete two factor confirmation for next sign in
-  //       await db.twoFactorConfirmation.delete({
-  //         where: { id: twoFactorConfirmation.id }
-  //       });
-  //     }
+      //       // Delete two factor confirmation for next sign in
+      //       await db.twoFactorConfirmation.delete({
+      //         where: { id: twoFactorConfirmation.id }
+      //       });
+      //     }
 
-  //     return true;
-  //   },
-  //   async session({ token, session }) {
-  //     if (token.sub && session.user) {
-  //       session.user.id = token.sub;
-  //     }
+      return true;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
 
-  //     if (token.role && session.user) {
-  //       session.user.role = token.role as UserRole;
-  //     }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      // console.log(session, "session here");
 
-  //     if (session.user) {
-  //       session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
-  //     }
+      // if (session.user) {
+      //   session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      // }
 
-  //     if (session.user) {
-  //       session.user.name = token.name;
-  //       session.user.email = token.email;
-  //       session.user.isOAuth = token.isOAuth as boolean;
-  //     }
+      if (session.user) {
+        session.user.name = token.name;
+        // session.user.email = token.email;
+        //   session.user.isOAuth = token.isOAuth as boolean;
+      }
 
-  //     return session;
-  //   },
-  //   async jwt({ token }) {
-  //     if (!token.sub) return token;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (!token.sub) return token;
 
-  //     const existingUser = await getUserById(token.sub);
+      if (!user) return token;
 
-  //     if (!existingUser) return token;
+      // console.log(token);
 
-  //     const existingAccount = await getAccountByUserId(
-  //       existingUser.id
-  //     );
+      // const existingAccount = await getAccountByUserId(
+      //   existingUser.id
+      // );
 
-  //     token.isOAuth = !!existingAccount;
-  //     token.name = existingUser.name;
-  //     token.email = existingUser.email;
-  //     token.role = existingUser.role;
-  //     token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
+      // token.isOAuth = !!existingAccount;
+      token.name = user.name;
+      token.email = user.email;
+      token.role = user.role;
+      // token.isTwoFactorEnabled = user.isTwoFactorEnabled;
 
-  //     return token;
-  //   }
-  // },
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
